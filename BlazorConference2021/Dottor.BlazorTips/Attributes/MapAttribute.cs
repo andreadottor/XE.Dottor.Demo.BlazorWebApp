414dc6697cbb5500bad7327c4fa38fa306960cdb
Andrea Dottor <andrea@dottor.net>
2021-04-12 19:01:54 +0200

    demo con reflection

---
+++ BlazorConference2021/Dottor.BlazorTips/Attributes/MapAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Dottor.BlazorTips.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MapAttribute : Attribute
+    {
+    }
+}
--- BlazorConference2021/Dottor.BlazorTips/Models/CardModel.cs
+++ BlazorConference2021/Dottor.BlazorTips/Models/CardModel.cs
@@ -1,13 +1,17 @@
 ﻿namespace Dottor.BlazorTips.Models
 {
     using System;
+    using Dottor.BlazorTips.Attributes;
     using Dottor.BlazorTips.Components.Sample2;
 
     public class CardModel
     {
 
         public Type ComponentType { get; set; } = typeof(BootstrapCard);
+
+        [Map]
         public string Title { get; set; }
+        [Map]
         public string Text { get; set; }
 
     }
