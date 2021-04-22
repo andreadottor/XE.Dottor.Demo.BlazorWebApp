c6d5f2f3dbe098c17f980b45b17dc7a6977c9f58
Andrea Dottor <andrea@dottor.net>
2021-04-22 15:51:33 +0200

    pulizia codice non usato

--- Dottor.ItalianCoders.Demo/Dottor.Earthquake/wwwroot/mapComponent.js
+++ Dottor.ItalianCoders.Demo/Dottor.Earthquake/wwwroot/mapComponent.js
@@ -5,12 +5,6 @@ export function showPrompt(message) {
   return prompt(message, 'Type anything here');
 }
 
-
-
-// TO MAKE THE MAP APPEAR YOU MUST ADD YOUR ACCESS TOKEN FROM 
-// https://account.mapbox.com
-/*mapboxgl.accessToken = '{ACCESS TOKEN}';*/
-
 export function addMapToElement(element, latitude, longitude) {
     var map = L.map(element).setView([latitude, longitude], 5);
 
@@ -20,11 +14,9 @@ export function addMapToElement(element, latitude, longitude) {
     }).addTo(map);
 
     var marker = L.marker([latitude, longitude]).addTo(map);
-
     return map;
 }
 
 export function setMapCenter(map, latitude, longitude) {
-    console.dir(map);
     map.setView([latitude, longitude], 5);
 }
